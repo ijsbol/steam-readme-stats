74c65ea33c7799a24288fa522a00272164891e32
ijsbol <hi@uwu.gal>
2024-12-06 15:29:25 +0000

    feat(api): register games badge endpoint

--- src/router.py
+++ src/router.py
@@ -3,6 +3,7 @@ import uvicorn
 
 from api._slug_.stats.router import router as api_slug_stats_router
 from api._slug_.stats.badge.playtime.router import router as api_slug_stats_badge_playtime_router
+from api._slug_.stats.badge.games.router import router as api_slug_stats_badge_games_router
 
 
 app = FastAPI()
@@ -10,6 +11,7 @@ app = FastAPI()
 
 app.include_router(api_slug_stats_router, prefix="/api/{steamid}/stats")
 app.include_router(api_slug_stats_badge_playtime_router, prefix="/api/{steamid}/stats/badge/playtime")
+app.include_router(api_slug_stats_badge_games_router, prefix="/api/{steamid}/stats/badge/games")
 
 
 uvicorn.run(app)
