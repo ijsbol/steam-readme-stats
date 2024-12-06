7d417eefd845cc027d3ad982b1b977a8d4b0aa8f
ijsbol <hi@uwu.gal>
2024-12-06 15:24:12 +0000

    chore(style): env style changes

--- src/env.py
+++ src/env.py
@@ -1,12 +1,12 @@
-__all__: tuple[str, ...] = (
-    "STEAM_API_KEY",
-)
-
-
 from os import getenv
 from typing import Final
 
 from dotenv import load_dotenv
+
+
+__all__: tuple[str, ...] = (
+    "STEAM_API_KEY",
+)
 
 
 load_dotenv()
