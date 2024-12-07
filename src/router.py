453fef83796f14362768df3b9a519989a80623cd
ijsbol <hi@uwu.gal>
2024-12-07 01:20:31 +0000

    feat(api): redirect to github repo on 404

--- src/router.py
+++ src/router.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 import uvicorn
 
 from api._slug_.stats.router import router as api_slug_stats_router
@@ -13,6 +15,11 @@ app.include_router(api_slug_stats_router, prefix="/api/{steamid}/stats")
 app.include_router(api_slug_stats_badge_playtime_router, prefix="/api/{steamid}/stats/badge/playtime")
 app.include_router(api_slug_stats_badge_games_router, prefix="/api/{steamid}/stats/badge/games")
 
+
+
+@app.exception_handler(HTTPStatus.NOT_FOUND)
+async def exception_not_found(request: Request) -> RedirectResponse:
+    return RedirectResponse(url="https://github.com/ijsbol/steam-readme-stats")
 
 
 if __name__ == "__main__":
